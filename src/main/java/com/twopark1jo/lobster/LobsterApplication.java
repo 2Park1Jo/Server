4f330f4d527fb6d9d4f0bdf2f586f2a301fea936
ming-taro <hera002008@naver.com>
2022-11-21 18:04:50 +0900

    [ADD] DB저장 전 ut8 인코딩

--- src/main/java/com/twopark1jo/lobster/LobsterApplication.java
+++ src/main/java/com/twopark1jo/lobster/LobsterApplication.java
@@ -1,6 +1,8 @@
 package com.twopark1jo.lobster;
 
+import com.twopark1jo.lobster.department.chat.ChatContentRepository;
 import com.twopark1jo.lobster.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class LobsterApplication {
 
 	public static void main(String[] args) {
