306dff60020b6c084027bc0136ae203d2ca827f1
ming-taro <hera002008@naver.com>
2023-05-28 18:28:03 +0900

    [ADD] 파파고 API requestbody값 확인 로그 추가

--- src/main/java/com/twopark1jo/lobster/translation/TranslationForm.java
+++ src/main/java/com/twopark1jo/lobster/translation/TranslationForm.java
@@ -2,9 +2,11 @@ package com.twopark1jo.lobster.translation;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class TranslationForm {
     private String source;
     private String target;
